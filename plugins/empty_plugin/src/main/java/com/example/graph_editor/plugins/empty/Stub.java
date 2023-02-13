b32cac038f2922876e3e0097b94e1da78fbb76eb
Piotr Kubaty <piotr.kubaty@student.uj.edu.pl>
2023-02-13 09:39:56 +0100

    add stub plugins for testing purposes

---
+++ plugins/empty_plugin/src/main/java/com/example/graph_editor/plugins/empty/Stub.java
@@ -0,0 +1,15 @@
+package com.example.graph_editor.plugins.empty;
+
+import graph_editor.extensions.Plugin;
+
+public class Stub implements Plugin {
+    @Override
+    public void activate(Proxy proxy) {
+
+    }
+
+    @Override
+    public void deactivate(Proxy proxy) {
+
+    }
+}
